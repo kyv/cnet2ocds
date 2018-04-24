23ef2437d601a69c365b532206dc57be38da7cc6
kevin <kevin@ki-ai.org>
2018-04-24 09:34:31 -0400

    allow for passing metadata to Release class
      * mostly usefull for package metadata
      * may help decouple tranformation from original data model

--- test/cnet2ocds.js
+++ test/cnet2ocds.js
@@ -6,35 +6,48 @@ const stateDocument = require('./data/cnetGEDocument.json');
 const cityDocument = require('./data/cnetGMDocument.json');
 const Release = require('../lib/ocds').default;
 
-const cnetDocument = cityDocument;
-
 describe('Transform compranet document to OCDS Release', () => {
 
   it('Release Package metadata should include publisher name', () => {
-    const ocdsPackage = new Release({cnetDocument}).package;
+    const ocdsPackage = new Release({
+      cnetDocument: cityDocument.body,
+      metadata: cityDocument,
+    }).package;
     should(ocdsPackage.publisher.name).eql('PODER');
   });
 
   it('Should have a release w/ ocid and an id', () => {
-    const release = new Release({cnetDocument: federalDocument}).release;
-    should(release.ocid).eql('OCDS-0UD2Q6-LA-019GYN059-T11-2012');
-    should(release.id).eql('LA-019GYN059-T11-2012');
+    const release = new Release({
+      cnetDocument: federalDocument.body,
+      metadata: federalDocument,
+    }).release;
+    should(release.ocid).eql('OCDS-0UD2Q6-AA-018TOQ765-N29-2012');
+    should(release.id).eql('AA-018TOQ765-N29-2012');
   });
 
   it('Release 0 should have array of parties', () => {
-    const release = new Release({cnetDocument: federalDocument}).release;
+    const release = new Release({
+      cnetDocument: federalDocument.body,
+      metadata: federalDocument,
+    }).release;
     const party = release.parties[0];
-    should(party.id).eql('instituto-de-seguridad-y-servicios-sociales-de-los-trabajadores-del-estado');
+    should(party.id).eql('comision-federal-de-electricidad');
   });
 
   it('Should validate release against OCDS schema', () => {
-    const r = new Release({cnetDocument});
+    const r = new Release({
+      cnetDocument: cityDocument.body,
+      metadata: cityDocument,
+    });
     const isValid = r.isValid;
     should(isValid).eql(true);
   });
 
   it('Should validate package against OCDS schema', () => {
-    const r = new Release({cnetDocument});
+    const r = new Release({
+      cnetDocument: cityDocument.body,
+      metadata: cityDocument,
+    });
     const isValid = r.isValidPackage;
     should(isValid).eql(true);
   });
