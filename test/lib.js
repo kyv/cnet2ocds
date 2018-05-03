43e7e8d47884fd5ce1ed8b84807a24ebf9c44e85
kevin <kevin@ki-ai.org>
2018-05-03 13:30:26 -0400

    test for case where date is missing seconds

--- test/lib.js
+++ test/lib.js
@@ -38,13 +38,16 @@ describe('Parse specific values', () => {
   it('should convert a date to ISO String', () => {
     const fechaFallo = '2013-06-22 00:00:00 GMT';
     const fechaCeleb = '2013-06-27 00:00:00';
+    const fechaNoSeconds = '2013-06-27 00:00';
     const fechaNoTime = '2013-06-27';
     const dateF = dateToISOString(fechaFallo);
     const dateC = dateToISOString(fechaCeleb);
     const dateN = dateToISOString(fechaNoTime);
+    const dateS = dateToISOString(fechaNoSeconds);
     should(dateF).eql('2013-06-22T00:00:00.000Z');
     should(dateC).eql('2013-06-27T00:00:00.000Z');
     should(dateN).eql('2013-06-27T00:00:00.000Z');
+    should(dateS).eql('2013-06-27T00:00:00.000Z');
   });
 
   it('UC name parser should return `name`, and `clave_uc` when APF', () => {
