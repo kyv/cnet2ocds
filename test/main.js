45b6b6ce50ff1b3d423d21b806980601985bae1a
kevin <kevin@ki-ai.org>
2018-05-11 16:30:00 -0400

    rename test/lib to test/parties

--- test/main.js
+++ test/main.js
@@ -1,4 +1,5 @@
 require = require('esm')(module/*, options*/);
 const ocds = require('./cnet2ocds');
 const tender = require('./tender');
+const parties = require('./parties');
 const lib = require('./lib');
